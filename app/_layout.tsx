f74ae7d4f9d0077cf519372f6f773a8f82e7585a
teyeong <taetae0202@naver.com>
2024-12-18 16:45:49 +0900

    feat: _layout.tsx에 Stack.Screen 라우팅 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="CalendarPage" options={{ headerShown: false }} />
+      <Stack.Screen name="TitlePage" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
